5b2eb698d8d47d4baac92d9fff8d951e00e2b781
Alexander Cortez <acortez8988@gmail.com>
2022-07-06 23:29:06 -0400

    commit tree f0f222ebf59b777ba4c7de95a58c5f3585f64ef9

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-// code your solution here
+// function razzle() {
+//     console.log("You've been razzled!");
+// }
+// razzle();
+
+
+function saturdayFun(activity= "roller-skate"){
+   return `This Saturday, I want to ${activity}!`
+}
+
+
+const mondayWork = function () {
+console.log(`This Monday, I will .`)
+}
